cffd854f99a3189ae04706759e771bfaaffba12e
kristenkozmary <kristenkozmary@yahoo.com>
2019-06-10 18:21:57 -0500

    working on it

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.use( express.static( "views" ) );
 
 app.set('view engine', 'ejs')
 
-
 app.get('/', function (req, res) {
     res.render('index');
 })
@@ -27,7 +26,7 @@ var dallas_id = 4684888;
   app.post('/', function (req, res) {
     
     var secret = process.env.API_KEY;
-console.log("made it to post");
+    console.log("made it to post");
   	//build api URL 
     const baseUrl = "http://api.openweathermap.org/data/2.5/group?id=";
 
