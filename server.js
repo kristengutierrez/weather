57a017c4f9cac798a67987df5678ed884550e5f9
kristenkozmary <kristenkozmary@yahoo.com>
2019-06-09 20:48:48 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const request = require('request')
+const app = express()
+
+var port = process.env.PORT || 8080
+
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+    res.render('index');
+})
+
+  app.post('/', function (req, res) {
+    let url = 'http://api.openweathermap.org/data/2.5/weather?id=2172797&units=imperial&appid=';
+  
+    request(url, function (err, response, body) {
+      if(err){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weather = JSON.parse(body)
+        if(weather.main == undefined){
+          res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+          res.render('index', {weather: weatherText, error: null});
+        }
+      }
+    });
+  })
+
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!')
+})
