d3d638ca8b2c3f272b62eeb4ed3034677ddc36e9
kristenkozmary <kristenkozmary@yahoo.com>
2019-06-10 07:38:06 -0500

    adding cities

--- server.js
+++ server.js
@@ -10,10 +10,21 @@ app.get('/', function (req, res) {
     res.render('index');
 })
 
+var milwaukee = "Milwaukee";
+var minneapolis = "Minneapolis";
+var chicago = "Chicago";
+var dallas = "Dallas";
+
+//city ids from openweathermap
+var milwaukee_id = 5263045;
+var minneapolis_id = 5037649;
+var chicago_id = 4887398;
+var dallas_id = 4684888;
+
   app.post('/', function (req, res) {
     var secret = process.env.API_KEY;
-
-    let url = 'http://api.openweathermap.org/data/2.5/weather?id=2172797&units=imperial&appid='+ secret;
+    let url = "http://api.openweathermap.org/data/2.5/group?id=" + milwaukee_id + "," + minneapolis_id + "," + chicago_id + "," + dallas_id + "&units=imperial&appid=" + secret;
+    //let url = 'http://api.openweathermap.org/data/2.5/weather?id=2172797&units=imperial&appid='+ secret;
   
     request(url, function (err, response, body) {
       if(err){
@@ -23,7 +34,7 @@ app.get('/', function (req, res) {
         if(weather.main == undefined){
           res.render('index', {weather: null, error: 'Error, please try again'});
         } else {
-          let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+          let weatherText = `It's ${weather.list[0].main.temp} degrees in ${weather.list[0].name}!`;
           res.render('index', {weather: weatherText, error: null});
         }
       }
@@ -32,5 +43,5 @@ app.get('/', function (req, res) {
 
 
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('weather app running')
 })
