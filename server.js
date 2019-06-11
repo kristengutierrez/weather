fdd5dae69176cc06d4c616eb69d0efa7d8b0cdef
kristenkozmary <kristenkozmary@yahoo.com>
2019-06-10 19:01:55 -0500

    cleaned up code. it's in a working state

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ app.set('view engine', 'ejs')
 var api = require('./routes/api');
 app.use('/', api);
 
-
-// app.get('/', function (req, res) {
-//     res.render('index');
-// })
-
 app.listen(port, function () {
   console.log('weather app running')
 })
